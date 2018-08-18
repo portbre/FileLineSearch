01213f5caca9152c2f7ecc37b5c8efe7e79a0dc1
portbre <barrhavenporters@yahoo.ca>
2018-08-18 15:42:37 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FileLineSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linenumber = 0;
+            if (args.Length == 2) 
+            {
+                if (File.Exists(args[0])) 
+                {
+                    foreach (string line in File.ReadLines(args[0], Encoding.UTF8))
+                    {
+                        // process the line
+                        linenumber++;
+                        if (line.IndexOf(args[1], StringComparison.CurrentCultureIgnoreCase) >= 0) {
+                            Console.WriteLine(linenumber.ToString() + ": " + line);
+                        }
+                    }
+                }
+                else 
+                {
+                    System.Console.WriteLine("Invalid Filename argument.");
+                    System.Console.WriteLine("Usage: FileLineSearch.exe InputFilename SearchText");
+                    return;
+                }
+            } 
+            else 
+            {
+                System.Console.WriteLine("Incorrect number of parameters.");
+                System.Console.WriteLine("Usage: FileLineSearch.exe InputFilename SearchText");
+                return;
+            }
+
+        }
+    }
+}
